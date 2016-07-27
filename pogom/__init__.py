6b40836e2f689cd00611b846c4a0440492aa88d5
1623tennyson <1623tennyson@gmail.com>
2016-07-27 12:05:08 +1200

    Update __init__.py

--- pogom/__init__.py
+++ pogom/__init__.py
@@ -5,5 +5,5 @@ config = {
     'LOCALE': 'en',
     'LOCALES_DIR': 'static/locales',
     'ROOT_PATH': None,
-    'GOOGLEMAPS_KEY': 'GMAPS KEY HERE'
+    'GOOGLEMAPS_KEY': 'AIzaSyBkbQI9p5chGHKHualKhhqEP0o0HOaSs6A'
 }
